03e48cccc08c9b8e894d60e09ac50a42e9105e2c
Devan Gwynn <devangwynn@gmail.com>
2024-12-05 03:58:06 -0700

    Update app

--- college_sports_app.py
+++ college_sports_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
-# import plotly.express as px
-# import matplotlib.pyplot as plt
-# import seaborn as sns
+import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 @st.cache_data
 def load_data(file_path):
@@ -12,9 +12,31 @@ def load_data(file_path):
 file_path = "all_stats.csv"
 all_stats_df = load_data(file_path)
 
-st.title("[INSERT TITLE HERE]")
+p4_conferences = {
+            "ACC": ["Boston College", "California", "Clemson", "Duke", "Florida St.", "Georgia Tech", "Louisville", "Miami (FL)", "North Carolina", "NC State", "Notre Dame", "Pittsburgh", "SMU", "Stanford", "Syracuse", "Virginia", "Virginia Tech", "Wake Forest"],
+            "Big Ten": ["Illinois", "Indiana", "Iowa", "Maryland", "Michigan", "Michigan St.", "Minnesota", "Nebraska", "Northwestern", "Ohio St.", "Oregon", "Penn St.", "Purdue", "Rutgers", "Southern California", "UCLA", "Washington", "Wisconsin"],
+            "Big 12": ["Arizona", "Arizona St.", "Baylor", "BYU", "Cincinnati", "Colorado", "Houston", "Iowa St.", "Kansas", "Kansas St.", "Oklahoma St.", "TCU", "Texas Tech", "UCF", "Utah", "West Virginia"],
+            "SEC": ["Alabama", "Arkansas", "Auburn", "Florida", "Georgia", "Kentucky", "LSU", "Mississippi St.", "Missouri", "Oklahoma", "Ole Miss", "South Carolina", "Tennessee", "Texas", "Texas A&M", "Vanderbilt"]
+            }
 
-overall_tab, football_tab, volleyball_tab, soccer_tab = st.tabs(["Overall", "Football", "Volleyball", "Soccer"])
+acc_teams = all_stats_df[all_stats_df["Team"].isin(p4_conferences["ACC"])]
+big_ten_teams = all_stats_df[all_stats_df["Team"].isin(p4_conferences["Big Ten"])]
+big_12_teams = all_stats_df[all_stats_df["Team"].isin(p4_conferences["Big 12"])]
+sec_teams = all_stats_df[all_stats_df["Team"].isin(p4_conferences["SEC"])]
+
+st.title("College Sports Stats")
+
+overall_tab, football_tab, volleyball_tab, soccer_tab = st.tabs(["Overall", "Football", "W Volleyball", "W Soccer"])
+
+def filter_colleges_by_conferences(df, selected_conferences):
+    if not selected_conferences:
+        return df
+    
+    selected_colleges = []
+    for conf in selected_conferences:
+        selected_colleges.extend(p4_conferences[conf])
+
+    return df[df["Team"].isin(selected_colleges)]
 
 def filter_colleges_by_sport(df, cols):
     return df.dropna(subset=cols)
@@ -31,6 +53,8 @@ with overall_tab:
     )
 
     if format_option == "All":
+        ave_z_score_plot = px.histogram(all_stats_df, x="Avg_Z_Score", title="Distribution of Average z-Score")
+        st.plotly_chart(ave_z_score_plot)
         with st.expander("See table"):
             all_stats_df.sort_values("Avg_Z_Score", ascending=False, inplace=True)
             all_stats_df.reset_index(drop=True, inplace=True)
@@ -38,18 +62,42 @@ with overall_tab:
             st.dataframe(all_stats_df)
 
     if format_option == "Conference":
-        p4_conferences_dict = {
-            "ACC": ["Boston College", "California", "Clemson", "Duke", "Florida St.", "Georgia Tech", "Louisville", "Miami (FL)", "North Carolina", "NC State", "Notre Dame", "Pittsburgh", "SMU", "Stanford", "Syracuse", "Virginia", "Virginia Tech", "Wake Forest"],
-            "Big Ten": ["Illinois", "Indiana", "Iowa", "Maryland", "Michigan", "Michigan St.", "Minnesota", "Nebraska", "Northwestern", "Ohio St.", "Oregon", "Penn St.", "Purdue", "Rutgers", "Southern California", "UCLA", "Washington", "Wisconsin"],
-            "Big 12": ["Arizona", "Arizona St.", "Baylor", "BYU", "Cincinnati", "Colorado", "Houston", "Iowa St.", "Kansas", "Kansas St.", "Oklahoma St.", "TCU", "Texas Tech", "UCF", "Utah", "West Virginia"],
-            "SEC": ["Alabama", "Arkansas", "Auburn", "Florida", "Georgia", "Kentucky", "LSU", "Mississippi St.", "Missouri", "Oklahoma", "Ole Miss", "South Carolina", "Tennessee", "Texas", "Texas A&M", "Vanderbilt"]
-            }
-        p4_conferences_list = p4_conferences_dict.keys
+               
         with st.popover("Select conference(s)"):
-            acc = st.checkbox("ACC")
-            big_ten = st.checkbox("Big Ten")
-            big_12 = st.checkbox("Big 12")
-            sec = st.checkbox("SEC")
+            acc = st.checkbox("ACC", key="acc_overall_checkbox")
+            big_ten = st.checkbox("Big Ten", key="big_ten_overall_checkbox")
+            big_12 = st.checkbox("Big 12", key="big_12_overall_checkbox")
+            sec = st.checkbox("SEC", key="sec_overall_checkbox")
+        
+        selected_conferences = []
+        selected_conference_teams = []
+
+        if acc:
+            selected_conferences.append("ACC")
+            selected_conference_teams.append(acc_teams)
+        if big_ten:
+            selected_conferences.append("Big Ten")
+            selected_conference_teams.append(big_ten_teams)
+        if big_12:
+            selected_conferences.append("Big 12")
+            selected_conference_teams.append(big_12_teams)
+        if sec:
+            selected_conferences.append("SEC")
+            selected_conference_teams.append(sec_teams)
+
+        if len(selected_conferences) != 0:
+            conference_df = filter_colleges_by_conferences(all_stats_df, selected_conferences)
+
+            plt.figure(figsize=(10, 6))
+            for conf_teams, conf_name in zip(selected_conference_teams, selected_conferences):
+                sns.kdeplot(data=conf_teams, x="Avg_Z_Score", label=conf_name)
+            plt.title("Average z-Score Distribution(s) for Selected Conference(s)")
+            plt.xlabel("Average z-Score")
+            plt.legend()
+            st.pyplot(plt)
+
+            with st.expander("See table"):
+                st.dataframe(conference_df)
 
     if format_option == "College":
         colleges = all_stats_df["Team"].sort_values()
@@ -62,43 +110,151 @@ with overall_tab:
         )
         college_df = all_stats_df[all_stats_df["Team"] == selected_college]
         if selected_college is not None:
+            # Display bar graph of each metric's z-score and average z-score for selected college
+            # college_metrics_df = college_df[["Team", ""]]
+            # overall_college_plot = px.bar(college_df, x=[""])
             st.dataframe(college_df)
-    
-    # If selected_college is None and selected conference is None, insert graph of average z-score distribution
-    # Else display bar graph of college's z-score for each metric and average z-score
 
 with football_tab:
     st.header("Football Stats")
-    # Insert check box for conference
-    # If selection is None, display graph of metric distribution for all teams
-    # If selection is one or more conferences, display graph of that conference's metric distribution
-    # Graph with checkbox for conferences and popover with checkbox for teams
+
+    with st.popover("Select conference(s)"):
+        acc = st.checkbox("ACC", key="acc_football_checkbox")
+        big_ten = st.checkbox("Big Ten", key="big_ten_football_checkbox")
+        big_12 = st.checkbox("Big 12", key="big_12_football_checkbox")
+        sec = st.checkbox("SEC", key="sec_football_checkbox")
+
+    selected_conferences = []
+    selected_conference_teams = []
+
+    if acc:
+        selected_conferences.append("ACC")
+        selected_conference_teams.append(acc_teams)
+    if big_ten:
+        selected_conferences.append("Big Ten")
+        selected_conference_teams.append(big_ten_teams)
+    if big_12:
+        selected_conferences.append("Big 12")
+        selected_conference_teams.append(big_12_teams)
+    if sec:
+        selected_conferences.append("SEC")
+        selected_conference_teams.append(sec_teams)
+
     football_cols = ["+/- PPG", "+/- PPG Z-Score"]
+    football_df = filter_colleges_by_sport(all_stats_df, football_cols)
+    football_df = filter_colleges_by_conferences(football_df, selected_conferences)
+    football_df.sort_values("+/- PPG", ascending=False, inplace=True)
+    football_df.reset_index(inplace=True)
+    football_df.index = football_df.index + 1
+
+    if len(selected_conferences) == 0:
+        football_metric_plot = px.histogram(football_df, x="+/- PPG", title="Distribution of +/- PPG for All Teams")
+        st.plotly_chart(football_metric_plot)
+
+    else:
+        plt.figure(figsize=(10, 6))
+        for conf_teams, conf_name in zip(selected_conference_teams, selected_conferences):
+            sns.kdeplot(data=conf_teams, x="+/- PPG", label=conf_name)
+        plt.title("Average +/- PPG Distribution(s) for Selected Conference(s)")
+        plt.xlabel("Average +/- PPG")
+        plt.legend()
+        st.pyplot(plt)
+    
     with st.expander("See table"):
-        football_df = filter_colleges_by_sport(all_stats_df, football_cols)
-        football_df.sort_values("+/- PPG", ascending=False, inplace=True)
-        football_df.reset_index(inplace=True)
-        football_df.index = football_df.index + 1
         st.dataframe(football_df[["Team"] + football_cols])
 
 with volleyball_tab:
-    st.header("Volleyball Stats")
-    # Insert graph
+    st.header("W Volleyball Stats")
+
+    with st.popover("Select conference(s)"):
+        acc = st.checkbox("ACC", key="acc_volleyball_checkbox")
+        big_ten = st.checkbox("Big Ten", key="big_ten_volleyball_checkbox")
+        big_12 = st.checkbox("Big 12", key="big_12_volleyball_checkbox")
+        sec = st.checkbox("SEC", key="sec_volleyball_checkbox")
+
+    selected_conferences = []
+    selected_conference_teams = []
+
+    if acc:
+        selected_conferences.append("ACC")
+        selected_conference_teams.append(acc_teams)
+    if big_ten:
+        selected_conferences.append("Big Ten")
+        selected_conference_teams.append(big_ten_teams)
+    if big_12:
+        selected_conferences.append("Big 12")
+        selected_conference_teams.append(big_12_teams)
+    if sec:
+        selected_conferences.append("SEC")
+        selected_conference_teams.append(sec_teams)
+
     volleyball_cols = ["+/- Hitting %", "+/- Hitting % Z-Score"]
+    volleyball_df = filter_colleges_by_sport(all_stats_df, volleyball_cols)
+    volleyball_df = filter_colleges_by_conferences(volleyball_df, selected_conferences)
+    volleyball_df.sort_values("+/- Hitting %", ascending=False, inplace=True)
+    volleyball_df.reset_index(inplace=True)
+    volleyball_df.index = volleyball_df.index + 1
+
+    if len(selected_conferences) == 0:
+        volleyball_metric_plot = px.histogram(volleyball_df, x="+/- Hitting %", title="Distribution of +/- Hitting Pct. for All Teams")
+        st.plotly_chart(volleyball_metric_plot)
+
+    else:
+        plt.figure(figsize=(10, 6))
+        for conf_teams, conf_name in zip(selected_conference_teams, selected_conferences):
+            sns.kdeplot(data=conf_teams, x="+/- Hitting %", label=conf_name)
+        plt.title("Average +/- Hitting Pct. Distribution(s) for Selected Conference(s)")
+        plt.xlabel("Average +/- Hitting Pct.")
+        plt.legend()
+        st.pyplot(plt)
+
     with st.expander("See table"):
-        volleyball_df = filter_colleges_by_sport(all_stats_df, volleyball_cols)
-        volleyball_df.sort_values("+/- Hitting %", ascending=False, inplace=True)
-        volleyball_df.reset_index(inplace=True)
-        volleyball_df.index = volleyball_df.index + 1
         st.dataframe(volleyball_df[["Team"] + volleyball_cols])
 
 with soccer_tab:
-    st.header("Soccer Stats")
-    # Insert graph
+    st.header("W Soccer Stats")
+
+    with st.popover("Select conference(s)"):
+        acc = st.checkbox("ACC", key="acc_soccer_checkbox")
+        big_ten = st.checkbox("Big Ten", key="big_ten_soccer_checkbox")
+        big_12 = st.checkbox("Big 12", key="big_12_soccer_checkbox")
+        sec = st.checkbox("SEC", key="sec_soccer_checkbox")
+
+    selected_conferences = []
+    selected_conference_teams = []
+
+    if acc:
+        selected_conferences.append("ACC")
+        selected_conference_teams.append(acc_teams)
+    if big_ten:
+        selected_conferences.append("Big Ten")
+        selected_conference_teams.append(big_ten_teams)
+    if big_12:
+        selected_conferences.append("Big 12")
+        selected_conference_teams.append(big_12_teams)
+    if sec:
+        selected_conferences.append("SEC")
+        selected_conference_teams.append(sec_teams)
+
     soccer_cols = ["+/- GPG", "+/- GPG Z-Score"]
+    soccer_df = filter_colleges_by_sport(all_stats_df, soccer_cols)
+    soccer_df = filter_colleges_by_conferences(soccer_df, selected_conferences)
+    soccer_df.sort_values("+/- GPG", ascending=False, inplace=True)
+    soccer_df.reset_index(inplace=True)
+    soccer_df.index = soccer_df.index + 1
+
+    if len(selected_conferences) == 0:
+        soccer_metric_plot = px.histogram(soccer_df, x="+/- GPG", title="Distribution of +/- GPG for All Teams")
+        st.plotly_chart(soccer_metric_plot)
+
+    else:
+        plt.figure(figsize=(10, 6))
+        for conf_teams, conf_name in zip(selected_conference_teams, selected_conferences):
+            sns.kdeplot(data=conf_teams, x="+/- GPG", label=conf_name)
+        plt.title("Average +/- GPG Distribution(s) for Selected Conference(s)")
+        plt.xlabel("Average +/- GPG")
+        plt.legend()
+        st.pyplot(plt)
+
     with st.expander("See table"):
-        soccer_df = filter_colleges_by_sport(all_stats_df, soccer_cols)
-        soccer_df.sort_values("+/- GPG", ascending=False, inplace=True)
-        soccer_df.reset_index(inplace=True)
-        soccer_df.index = soccer_df.index + 1
         st.dataframe(soccer_df[["Team"] + soccer_cols])
